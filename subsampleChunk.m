cb068ed1546118a78915591d0f91e8cff95454ea
aojeda <alejo.ojeda83@gmail.com>
2013-10-01 17:13:30 -0700

    subsampleChunk.m bug fixed

--- subsampleChunk.m
+++ subsampleChunk.m
@@ -1,6 +1,6 @@
 function [values,timestamps,stream] = subsampleChunk(values,timestamps,stream,~)
 try %#ok
-    if str2double(stream.info.nominal_srate) > 20e3
+    if str2double(stream.info.nominal_srate) > 2e4
         values = values(:,1:2:end);
         timestamps = timestamps(1:2:end);
     end
