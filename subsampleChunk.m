d6b179b60a7d8c835c41a303a25088f1ddee09a6
aojeda <alejo.ojeda83@gmail.com>
2019-03-12 22:46:54 -0700

    Removing obsolete functions

--- subsampleChunk.m
+++
@@ -1,7 +0,0 @@
-function [values,timestamps,stream] = subsampleChunk(values,timestamps,stream,~)
-try %#ok
-    if str2double(stream.info.nominal_srate) > 2e4
-        values = values(:,1:2:end);
-        timestamps = timestamps(1:2:end);
-    end
-end
--- tStudent2Dmap.m
+++
@@ -1,4 +0,0 @@
-function t = tStudent2Dmap(x)
-n = size(x,1);
-t = mean(x)./(std(x)+eps)/sqrt(n);
-t = squeeze(t);
